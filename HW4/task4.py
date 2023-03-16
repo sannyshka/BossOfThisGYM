920cd00101afcf248a42177c79b294f322faefc8
Sannyshka <s.alexandr.valerievich1997@gmail.com>
2023-03-16 18:57:18 +0200

    Домашня робота 4 готова.

---
+++ HW4/task4.py
@@ -0,0 +1,13 @@
+meat = str(input("Яке м'ясо краще використовувати для приготування шашлику? "))
+match meat:
+    case "Телятина":
+        print("Смачно, але не те.")
+        print(f"Ви ввели дані: {type(meat)}")
+        print(f"В рядку {len(meat)} символів")
+    case "Свинина":
+        print("Це найкраще м'ясо для шашлику.")
+        print(f"Ви ввели дані: {type(meat)}")
+        print(f"В рядку {len(meat)} символів")
+    case _:
+        print("Ні")
+        print(f"Ви ввели дані: {type(meat)}")
